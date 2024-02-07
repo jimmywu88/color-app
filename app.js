4344ba88827117bf2d2ce08f80fcd9653a6b5825
jimmywu88 <94825422+jimmywu88@users.noreply.github.com>
2024-02-07 14:06:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.set('view engine', 'ejs');
 //   message: 'Hello',
 // }
 function getStyleFromEnv() {
-  const backgroundColor = process.env.BG_COLOR || 'green';
+  const backgroundColor = process.env.BG_COLOR || 'black';
   const foregroundColor = process.env.FG_COLOR || '#FFFFFF';
-  const message = process.env.MESSAGE || 'Hello World! SS2 v2';
+  const message = process.env.MESSAGE || 'Hello World! SS2 v1';
 
   return {
     styleClass: `background-color:${backgroundColor}; color:${foregroundColor}`,
